bc07c46b7ba8f124d42966634f2b325133adc60e
tadokoro <tadokoro0216@gmail.com>
2022-05-22 04:32:28 +0000

    follow

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'department_id', 'course_id'
     ];
 
     /**
@@ -37,15 +37,63 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
     
+    public function course() {
+        return $this->belongsTo(Course::class);
+    }
+    
+    public function department() {
+        return $this->belongsTo(Department::class);
+    }
+    
     public function questions() {
         return $this->hasMany(Question::class);
     }
     
     public function answers() {
-        return $this->hasMany(Answer::class);
+        return $this->hasMany(Answer::class)->orderBy('created_at', 'desc');
+    }
+    
+    public function followings() {
+        return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
+    }
+    
+    public function followers() {
+        return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
+    }
+    
+    public function follow($userId) {
+        $exist = $this->is_following($userId);
+        
+        $its_me = $this->id == $userId;
+        
+        if($exist || $its_me) {
+            return false;
+        }
+        else {
+            $this->followings()->attach($userId);
+            return true;
+        }
+    }
+    
+    public function unfollow($userId) {
+        $exist = $this->is_following($userId);
+        
+        $its_me = $this->id == $userId;
+        
+        if($exist && !$its_me) {
+            $this->followings()->detach($userId);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    public function is_following($userId) {
+        return $this->followings()->where('follow_id', $userId)->exists();
     }
     
     public function loadRelationshipCounts() {
-        $this->loadCount(['questions', 'answers']);
+        $this->loadCount(['questions', 'answers', 'followings', 'followers']);
     }
 }
