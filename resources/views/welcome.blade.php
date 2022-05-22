a899f23dbaa04afe99a8a1a64cb00ceda3910011
tadokoro <tadokoro0216@gmail.com>
2022-05-22 04:43:30 +0000

    modified welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,14 +2,56 @@
 
 @section('content')
     @if(Auth::check())
-        <h1>ログイン済み</h1>
-    @else
-        <div class="center jumbotron mt-5">
-            <div class="text-center">
-                <h1>QuestionBoard</h1>
+        <div class="row mt-5">
+            <div class="col-sm-7">
+                <div class="row">
+                    <div class="ml-4">
+                        <h2>質問一覧</h2>
+                    </div>
+                    
+                    <div class="ml-3">
+                        {!! link_to_route('questions.create', '質問する', ['id' => $profile_user->id], ['class' => 'btn btn-primary']) !!}
+                    </div>
+                </div>
                 
-                {!! link_to_route('signup.get', '会員登録', [], ['class' => 'btn btn-primary']) !!}
+                <table class="table table-bordered bg-white question-title">
+                    @foreach($questions as $question)
+                        <tr>
+                            <th><a href="{{ route('questions.show', ['question' => $question->id]) }}" class="text-truncate" style="max-width: 633px;">{{ $question->title }}</a></th>
+                        </tr>
+                    @endforeach
+                </table>
+                
+                {{ $questions->links() }}
+            </div>
+            
+            <div class="col-sm-4 offset-sm-1 bg-white" style="height: 500px;">
+                @include('users.profile')
             </div>
         </div>
+    @else
+    <div class="mt-3">
+        <div class="text-center mb-5">
+            <h2>ログイン</h2>
+        </div>
+        
+        {!! Form::open(['route' => 'login.post']) !!}
+            <div class="form-group">
+                {!! Form::label('email', 'メールアドレス：') !!}
+                {!! Form::email('email', null, ['class' => 'form-control']) !!}
+            </div>
+            
+            <div class="form-group">
+                {!! Form::label('password', 'パスワード：') !!}
+                {!! Form::password('password', ['class' => 'form-control']) !!}
+            </div>
+            
+            <div class="form-group">
+                {!! Form::submit('ログイン', ['class' => 'btn btn-primary btn-block']) !!}
+            </div>
+        {!! Form::close() !!}
+        
+        <p>会員登録がお済みでない方は、{!! link_to_route('signup.get', 'こちら') !!}</p>
+    </div>
     @endif
 @endsection
